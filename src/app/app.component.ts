42a632f9c424847d9f53e74038b151d9d79c7a1f
Vitaly Savchuk <vitalysavchuk2312@gmail.com>
2020-10-27 01:28:54 +0200

    first step in correct logic implementing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,78 +18,65 @@ export class AppComponent {
   flagsArr = [
     {
       src: './assets/flags/flag1.png',
-      offsetX: 0
+      offsetX: 0,
+      offsetY: 0,
     },
     {
       src: './assets/flags/flag2.png',
-      offsetX: 30
+      offsetX: 30,
+      offsetY: 0,
     },
     {
       src: './assets/flags/flag3.png',
-      offsetX: 60
+      offsetX: 60,
+      offsetY: 0,
     },
     {
       src: './assets/flags/flag4.png',
-      offsetX: 90
+      offsetX: 90,
+      offsetY: 0,
     },
     {
       src: './assets/flags/flag5.png',
-      offsetX: 120
+      offsetX: 120,
+      offsetY: 0,
     },
     {
       src: './assets/flags/flag6.png',
-      offsetX: 90
+      offsetX: 90,
+      offsetY: 0,
     },
     {
       src: './assets/flags/flag7.png',
-      offsetX: 60
+      offsetX: 60,
+      offsetY: 0,
     },
     {
       src: './assets/flags/flag8.png',
-      offsetX: 30
+      offsetX: 30,
+      offsetY: 0,
     },
     {
       src: './assets/flags/flag9.png',
-      offsetX: 0
+      offsetX: 0,
+      offsetY: 0,
     },
   ]
 
-  removeFlag(flag, index) {
-    let removedFlagOffsetX = flag.offsetX;
-    this.flagsArr = this.flagsArr.filter(item => item !== flag);
+  removeFlag(removedFlag, index) {
+    this.flagsArr = this.flagsArr.filter(item => item !== removedFlag);
+    const flagsLength = this.flagsArr.length;
     let temp;
-    for (let i = index; i < this.flagsArr.length; i++) {
-      const currentFlag = this.flagsArr[i];
-      const nextFlag = this.flagsArr[i + 1];
-      if (i === index) {
-        // first iteration
-        temp = currentFlag.offsetX;
-        currentFlag.offsetX = removedFlagOffsetX;
+    for (let i = index; i < flagsLength; i++) {
+      let currentFlagOffset = this.flagsArr[i].offsetX;
+      if (i === index) { // first iteration
+        temp = currentFlagOffset;
+        this.flagsArr[i].offsetX = removedFlag.offsetX;
         continue;
       }
-      if (nextFlag) {
-        currentFlag.offsetX = temp;
-        temp = nextFlag.offsetX;
-      }
+      let temp2 = currentFlagOffset;
+      this.flagsArr[i].offsetX = temp;
+      temp = temp2;
     }
-  }
- 
-  setOffset(i) {
-    const offsetX = i * 50;
-    const offsetY = i * 10;
-    if (this.flagsArr.length - i < i) {
-      const newOffset = this.flagsArr[i - this.appropriateElementIndex].offsetX;
-      let styles = {
-        'transform': 'translate(-' + newOffset + 'px,-' + offsetY + 'px)'
-      }
-
-        // only for first initialisation.
-      return styles;
-    }
-    let styles = {
-      'transform': 'translate(-' + offsetX + 'px,-' + offsetY + 'px)'
-    }
-    this.flagsArr[i].offsetX = offsetX;
-    return styles;
   }
 }
