528a5af737db2b21fb77ccfdcb7842d0055aa2f2
Vitaly Savchuk <vitalysavchuk2312@gmail.com>
2020-11-01 18:08:35 +0200

    implemented final version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,79 +11,62 @@ export class AppComponent {
   heading = 'Cristofer Carder Junior';
   faStar = faStar;
   faStarHalf = faStarHalf;
+  startTopPosition = 270;
   flagsArr = [
     {
       src: './assets/flags/flag1.png',
-      offsetX: 0
+      offset: 0
     },
     {
       src: './assets/flags/flag2.png',
-      offsetX: -30
+      offset: 0
     },
     {
       src: './assets/flags/flag3.png',
-      offsetX: -60
+      offset: 0
     },
     {
       src: './assets/flags/flag4.png',
-      offsetX: -90
+      offset: 0
     },
     {
       src: './assets/flags/flag5.png',
-      offsetX: -120
+      offset: 0
     },
     {
       src: './assets/flags/flag6.png',
-      offsetX: -90
+      offset: 0
     },
     {
       src: './assets/flags/flag7.png',
-      offsetX: -60
+      offset: 0
     },
     {
       src: './assets/flags/flag8.png',
-      offsetX: -30
+      offset: 0
     },
     {
       src: './assets/flags/flag9.png',
-      offsetX: 0
+      offset: 0
     },
   ]
 
-  removeFlag(removedFlag, index) {
-    this.flagsArr = this.flagsArr.filter(item => item !== removedFlag);
-    const flagsLength = this.flagsArr.length;
-    let temp;
-    for (let i = index; i < flagsLength; i++) {
-      let currentFlagOffset = this.flagsArr[i].offsetX;
-      if (i === index) { // first iteration
-        temp = currentFlagOffset;
-        this.flagsArr[i].offsetX = removedFlag.offsetX;
-        continue;
-      }
-      let temp2 = currentFlagOffset;
-      this.flagsArr[i].offsetX = temp;
-      temp = temp2;
-    }
-    if (flagsLength % 2 === 0) {
-      const startIndex = flagsLength / 2;
-      let relativeFlag = startIndex - 1;
-      this.alignFlags(startIndex, relativeFlag);
-      for (let i = 0; i < flagsLength; i++) {
-        this.flagsArr[i].offsetX -= 30;
-      }
-    } else {
-      const startIndex = Math.round(flagsLength / 2);
-      let relativeFlag = startIndex - 2;
-      this.alignFlags(startIndex, relativeFlag);
+  ngOnInit() {
+    if (this.flagsArr.length === 9) { // do it only during first initialistion
+      this.alignItems();
     }
   }
 
-  alignFlags(startIndex: number, relativeFlag: number) {
-    const flagsLength = this.flagsArr.length;
-    for (let i = startIndex; i < flagsLength; i++) {
-      this.flagsArr[i].offsetX = this.flagsArr[relativeFlag].offsetX;
-      relativeFlag--;
+  removeFlag(removedFlag) {
+    this.flagsArr = this.flagsArr.filter(item => item !== removedFlag);
+    this.startTopPosition -= 10;
+    this.alignItems();
+  }
+
+  alignItems() {
+    this.flagsArr[0].offset = this.startTopPosition;
+    for (let i = 1; i < this.flagsArr.length; i++) {
+      this.flagsArr[i].offset = this.flagsArr[i - 1].offset - 22;
     }
   }
 
